a5c98e18dbbfebe0fe7d766a1cf72353d48aac5a
Zach Marbach <Zach.Marbach@improving.com>
2019-09-17 11:54:16 -0500

    tag changes

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -1,7 +1,25 @@
 package org.improving.tag;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print("> ");
+            String input = scanner.nextLine().trim()
+            if (input.equals("look")) {
+                System.out.println("You look around.");
+            }
+            if (input.equals("inventory")){
+                System.out.println("You are carrying nothing.");
+            }
+            if (input.equals("dance")) {
+                System.out.println("You dance around.");
+            }
+            if (input.equals("jump")) {
+                System.out.println("You jump in the air.");
+            }
+        }
     }
 }
