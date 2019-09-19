65fdf29238f882548d4588d459d3e177e934f481
Zach Marbach <Zach.Marbach@improving.com>
2019-09-19 15:10:40 -0500

    cleaning up Game by adding BaseEmote array.

--- src/main/java/org/improving/tag/Game.java
+++ src/main/java/org/improving/tag/Game.java
@@ -1,9 +1,6 @@
 package org.improving.tag;
 
-import org.improving.tag.commands.DanceCommand;
-import org.improving.tag.commands.InventoryCommand;
-import org.improving.tag.commands.JumpCommand;
-import org.improving.tag.commands.LookCommand;
+import org.improving.tag.commands.*;
 
 import java.util.Date;
 import java.util.Scanner;
@@ -11,12 +8,31 @@ import java.util.Scanner;
 public class Game {
     private Date startTime;
     private Date endTime;
+    private BaseEmoteCommand[] Commands;
 
-    public Date getStartTime() { return startTime; }
-    private void setStartTime(Date startTime) {this.startTime = startTime;}
+    public Game() {
+        Commands = new BaseEmoteCommand[]{
+                new LookCommand(),
+                new DanceCommand(),
+                new InventoryCommand(),
+                new JumpCommand()};
+    }
 
-    public Date getEndTime() { return endTime; }
-    private void setEndTime(Date endTime) { this.endTime = endTime; }
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    private void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    private void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
@@ -26,25 +42,26 @@ public class Game {
         while (loop) {
             System.out.print("> ");
             String input = scanner.nextLine().trim();
-            LookCommand lCmd = new LookCommand();
-            DanceCommand dCmd = new DanceCommand();
-            InventoryCommand iCmd = new InventoryCommand();
-            JumpCommand jCmd = new JumpCommand();
-            if (lCmd.isValid(input)) {
-                lCmd.execute(input);
-            } else if (jCmd.isValid(input)) {
-                jCmd.execute(input);
-            } else if (dCmd.isValid(input)) {
-                dCmd.execute(input);
-            } else if (iCmd.isValid(input)) {
-                iCmd.execute(input);
+            BaseEmoteCommand validCommand = getValidCommand(input);
+            if (null != validCommand) {
+                validCommand.execute(input);
             } else if (input.equals("exit")) {
-            System.out.println("Goodbye.");
-            loop = false;
+                System.out.println("Goodbye.");
+                loop = false;
             } else {
                 System.out.println("Huh? I don't understand.");
             }
         }
         this.setEndTime(new Date());
+    }
+
+    private BaseEmoteCommand getValidCommand(String input) {
+        BaseEmoteCommand validCommand = null;
+        for (BaseEmoteCommand command : Commands) {
+            if (command.isValid(input)) {
+                return command;
+            }
+        }
+        return null;
     }
 }
